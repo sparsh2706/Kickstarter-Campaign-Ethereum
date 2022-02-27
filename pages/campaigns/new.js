560e39dcad7b2b8996da6b7d5d4acb531eaa59cf
Sparsh Dutta <sparsh@Sparshs-MacBook-Pro.local>
2022-02-28 00:07:01 +0530

    Creating a Form Submit Event handler

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,17 +1,41 @@
 import React, { Component } from "react";
-import { Button, Form } from "semantic-ui-react";
+import { Button, Form, Input } from "semantic-ui-react";
 import Layout from "../../components/Layout";
+import factory from '../../ethereum/factory';
+import web3 from '../../ethereum/web3';
 
 class CampaignNew extends Component {
+    state = {
+        minimumContribution: ''
+    };
+
+    onSubmit = async (event) => {
+        event.preventDefault() // Prevents auto submission of Form to the backend server
+
+        const accounts = await web3.eth.getAccounts();
+        await factory.methods
+                .createCampaign(this.state.minimumContribution)
+                .send({
+                   from: accounts[0]
+                });
+        /* We dont have to specifiy the Gas since Metamask takes care of it */
+    };
+
     render() {
         return (
             <Layout>
                 <h3>Create a Campaign</h3>
-
-                <Form>
+                
+                <Form onSubmit={this.onSubmit}> 
                     <Form.Field>
                         <label>Minimum Contribution</label>
-                        <input />
+                        <Input
+                            label='Wei'
+                            labelPosition="right"
+                            value={this.state.minimumContribution}
+                            onChange={event =>
+                                this.setState({ minimumContribution: event.target.value })}
+                        />
                     </Form.Field>
                     <Button primary>Create!</Button>
                 </Form>
@@ -21,3 +45,8 @@ class CampaignNew extends Component {
 }
 
 export default CampaignNew;
+
+/* We didnt pass parentheses in this.onSubmit since doing that
+would mean that the function would get executed at that
+same moment. We just pass a reference to the function
+that it should get executed when we need it to */
