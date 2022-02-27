5468358b551bb27d2059831e90f858efd8b03ed1
Sparsh Dutta <sparsh@Sparshs-MacBook-Pro.local>
2022-02-27 14:05:35 +0530

    Addded HEAD tag of nextjs to include the CSS CDN

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react';
+import Head from 'next/head';
 import Header from './Header';
 const Layout = (props) => { // Functional Components gets called with props
     return (
         // Adding COntainer to fix up margins
         <Container> 
+            <Head>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" integrity="sha512-8bHTC73gkZ7rZ7vpqUQThUDhqcNFyYi2xgDgPDHc+GXVGHXq+xPjynxIopALmOPqzo9JZj0k6OqqewdGO3EsrQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+            </Head>
             <Header />
             {props.children}
         </Container>
@@ -12,6 +16,11 @@ const Layout = (props) => { // Functional Components gets called with props
 };
 
 export default Layout;
+
+
+/* Everything inside the Head Tag of Nextjs would lead
+it to write the bunch of code inside it in the HTML
+Head tag <head> */
 
 /* Layout would be used in all the pages, so its like 
 a Maste Blade file */
--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,6 @@ class CampaignIndex extends Component {
         return (
         <Layout>
             <div>
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" integrity="sha512-8bHTC73gkZ7rZ7vpqUQThUDhqcNFyYi2xgDgPDHc+GXVGHXq+xPjynxIopALmOPqzo9JZj0k6OqqewdGO3EsrQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
                 <h3>Open Campaigns</h3>
                 <Button 
                     floated='right' // the button gets floated to the right side 
