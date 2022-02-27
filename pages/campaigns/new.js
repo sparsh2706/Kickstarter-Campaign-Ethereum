454bf82048435334df58ed8e2a7621881175e725
Sparsh Dutta <sparsh@Sparshs-MacBook-Pro.local>
2022-02-27 14:10:01 +0530

    Added the Form Boilerplate

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
+import { Button, Form } from "semantic-ui-react";
 import Layout from "../../components/Layout";
 
 class CampaignNew extends Component {
     render() {
         return (
             <Layout>
-                
+                <h3>Create a Campaign</h3>
+
+                <Form>
+                    <Form.Field>
+                        <label>Minimum Contribution</label>
+                        <input />
+                    </Form.Field>
+                    <Button primary>Create!</Button>
+                </Form>
             </Layout>
         );
     }
