5d41b8e0944e30f6a284e340b82c6e2ca5e52562
Sparsh Dutta <sparsh@Sparshs-MacBook-Pro.local>
2022-02-28 00:18:52 +0530

    Added Form Validation in create campaign

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,24 +1,29 @@
 import React, { Component } from "react";
-import { Button, Form, Input } from "semantic-ui-react";
+import { Button, Form, Input, Message } from "semantic-ui-react";
 import Layout from "../../components/Layout";
 import factory from '../../ethereum/factory';
 import web3 from '../../ethereum/web3';
 
 class CampaignNew extends Component {
     state = {
-        minimumContribution: ''
+        minimumContribution: '',
+        errorMessage: ''
     };
 
     onSubmit = async (event) => {
         event.preventDefault() // Prevents auto submission of Form to the backend server
 
-        const accounts = await web3.eth.getAccounts();
-        await factory.methods
-                .createCampaign(this.state.minimumContribution)
-                .send({
-                   from: accounts[0]
-                });
-        /* We dont have to specifiy the Gas since Metamask takes care of it */
+        try {
+            const accounts = await web3.eth.getAccounts();
+            await factory.methods
+                    .createCampaign(this.state.minimumContribution)
+                    .send({
+                       from: accounts[0]
+            });
+            /* We dont have to specifiy the Gas since Metamask takes care of it */
+        } catch (err) {
+            this.setState({ errorMessage: err.message });
+        }
     };
 
     render() {
@@ -26,7 +31,7 @@ class CampaignNew extends Component {
             <Layout>
                 <h3>Create a Campaign</h3>
                 
-                <Form onSubmit={this.onSubmit}> 
+                <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}> 
                     <Form.Field>
                         <label>Minimum Contribution</label>
                         <Input
@@ -37,6 +42,8 @@ class CampaignNew extends Component {
                                 this.setState({ minimumContribution: event.target.value })}
                         />
                     </Form.Field>
+
+                    <Message error header="Oops!" content={this.state.errorMessage} />
                     <Button primary>Create!</Button>
                 </Form>
             </Layout>
@@ -50,3 +57,14 @@ export default CampaignNew;
 would mean that the function would get executed at that
 same moment. We just pass a reference to the function
 that it should get executed when we need it to */
+
+/* We had to add the 'error' prop in the Form tag as well
+so that Semantic UI can display the message */
+
+/* error={!!this.state.errorMessage}, when the string is empty
+it is meant as error=False, so no error Message would be shown
+with Oops.
+The two exclamations is a little trick
+!!"" => False
+!!"Any String" => True
+*/
