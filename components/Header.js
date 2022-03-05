848e9e3f7561567c4e66d1b0fc56b9385d29ea93
Sparsh Dutta <sparsh@Sparshs-MacBook-Pro.local>
2022-03-05 10:13:38 +0530

    Added Link tag for header navigation

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Menu } from "semantic-ui-react";
+import { Link } from '../routes';
 
 /* This is a Functional Component */
 /* All this Semantic UI specific stuff can be found in its docs */
@@ -8,17 +9,24 @@ const Header = () => {
         
         <Menu style = {{ marginTop: '10px' }}>
             
-            <Menu.Item>
-                CrowdCoin
-            </Menu.Item>
+            <Link route="/">
+                <a className="item">
+                    CrowdCoin
+                </a>
+            </Link>
             
             <Menu.Menu position="right">
-                <Menu.Item>
-                    Campaigns
-                </Menu.Item>
-                <Menu.Item>
-                    +
-                </Menu.Item>
+                <Link route="/">
+                    <a className="item">
+                        Campaigns
+                    </a>
+                </Link>
+
+                <Link route="/campaigns/new">
+                    <a className="item">
+                        +
+                    </a>
+                </Link>
             </Menu.Menu>
         
         </Menu>
@@ -27,8 +35,10 @@ const Header = () => {
 
 export default Header;
 
+/* Since Menu.Item Tag and Link Tag Styling Component looks kinda similar
+and it would mess up the CSS of the website. We might as well remove Menu.item */
+
 /* Two sets of Curly braces in styles = {{}} means:
     1st Brace: That we are going to write some JS object
     2nd Brace: The actual object
-
 */
