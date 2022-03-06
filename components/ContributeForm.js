69c9ff49c78a7d2d571dc49af469f0f60fbc9764
Sparsh Dutta <sparsh@Sparshs-MacBook-Pro.local>
2022-03-06 11:00:04 +0530

    Using the COntract mehthod to contribute and adding Refresh Route

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Button, Form, Input, Message } from 'semantic-ui-react';
 import Campaign from '../ethereum/campaign';
+import web3 from '../ethereum/web3';
+import { Router } from '../routes';
 
 class ContributeForm extends Component {
 
@@ -8,10 +10,28 @@ class ContributeForm extends Component {
         value: ''
     };
 
-    onSubmit = (event) => {
+    onSubmit = async (event) => {
         event.preventDefault();
 
         const campaign = Campaign(this.props.address);
+
+        try {
+            /* Calling Contract Method to contribute in Wei */
+            const accounts = await web3.eth.getAccounts();
+            await campaign.methods.contribute().send({
+                from: accounts[0],
+                value: web3.utils.toWei(this.state.value, 'ether')
+            });
+            /* We didnt use pushRoute since that would redirect to the page i.e would
+            add the URL in the History of the browser, essentially which means is that the user clicks
+            the Back button, but we only want to refresh the page.
+            For that we use replaceRoute */
+            Router.replaceRoute(`/campaigns/${this.props.address}`)
+
+        } catch (err) {
+
+        }
+
     }
 
     render() {
