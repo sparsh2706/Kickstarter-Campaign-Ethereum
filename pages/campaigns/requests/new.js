d4e23e48cb476eb117bea2c5998c5625bc92887c
Sparsh Dutta <sparsh@Sparshs-MacBook-Pro.local>
2022-03-11 01:08:02 +0530

    Adding Error and Loading Handler

--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -10,7 +10,9 @@ class RequestNew extends Component {
     state = {
         value: '',
         description: '',
-        recipient: ''
+        recipient: '',
+        loading: false,
+        errorMessage: ''
     };
 
     static async getInitialProps(props) {
@@ -25,6 +27,8 @@ class RequestNew extends Component {
         const campaign = Campaign(this.props.address);
         const { description, value, recipient } = this.state;
 
+        this.setState({ loading: true, errorMessage: '' });
+
         try {
             const accounts = await web3.eth.getAccounts();
             await campaign.methods
@@ -34,16 +38,23 @@ class RequestNew extends Component {
                     recipient
                 )
                 .send({ from: accounts[0] });
+            Router.pushRoute(`/campaigns/${this.props.address}/requests`);
         } catch (err) {
-
+            this.setState({ errorMessage: err.message });
         }
+        this.setState({ loading: false });
     }
 
     render() {
         return(
             <Layout>
+                <Link route={`/campaigns/${this.props.address}/requests`}>
+                    <a>
+                        Back
+                    </a>
+                </Link>
                 <h3>Create a Request</h3>
-                <Form onSubmit={this.onSubmit}>
+                <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
                     <Form.Field>
                         <label>Description</label>
                         <Input
@@ -65,7 +76,8 @@ class RequestNew extends Component {
                             onChange={event => this.setState({ recipient: event.target.value })}
                         />
                     </Form.Field>
-                    <Button primary>
+                    <Message error header='Oops!' content={this.state.errorMessage}></Message>
+                    <Button primary loading={this.state.loading}>
                         Create!
                     </Button>
                 </Form>
