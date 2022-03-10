353e2a1b8d473c78f52ae5b92c5611ee4d2759d9
Sparsh Dutta <sparsh@Sparshs-MacBook-Pro.local>
2022-03-11 00:37:06 +0530

    Creating the New Request Form

--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -1,9 +1,57 @@
 import React, { Component } from 'react';
+import { Form, Button, Message, Input } from 'semantic-ui-react';
+import Campaign from '../../../ethereum/campaign';
+import web3 from '../../../ethereum/web3';
+import { Link, Router } from '../../../routes';
+import Layout from '../../../components/Layout';
 
 class RequestNew extends Component {
+
+    state = {
+        value: '',
+        description: '',
+        recepient: ''
+    };
+
+    static async getIntialProps(props) {
+        const { address } = props.query;
+
+        return { address: address };
+    }
+
     render() {
         return(
-            <h3>Create a Request</h3>
+            <Layout>
+                <h3>Create a Request</h3>
+                <Form>
+                    <Form.Field>
+                        <label>Description</label>
+                        <Input
+                            value={this.state.description}
+                            onChange={event => this.setState({ description: event.target.value })}
+                        />
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Value in Ether</label>
+                        <Input
+                            value={this.state.value}
+                            onChange={event => this.setState({ value: event.target.value })}
+                        />
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Recepient</label>
+                        <Input
+                            value={this.state.recepient}
+                            onChange={event => this.setState({ recepient: event.target.value })}
+                        />
+                    </Form.Field>
+                    <Button primary>
+                        Create!
+                    </Button>
+                </Form>
+
+
+            </Layout>
         );
     }
 }
