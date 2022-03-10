561402da000ffc70169b44f3b49c4047f9366067
Sparsh Dutta <sparsh@Sparshs-MacBook-Pro.local>
2022-03-11 01:23:09 +0530

    Getting the List of All Requests

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 import { Button } from 'semantic-ui-react';
 import { Link } from '../../../routes';
 import Layout from '../../../components/Layout';
+import Campaign from '../../../ethereum/campaign';
 
 class RequestIndex extends Component {
 
     static async getInitialProps(props) {
         const { address } = props.query;
-        return { address: address };
+        const campaign = Campaign(address);
+        const requestCount = await campaign.methods.getRequestCount().call();
+
+        /* Explanation below */
+        const requests = await Promise.all(
+            Array(requestCount)
+                .fill()
+                .map((element, index) => {
+                    return campaign.methods.requests(index).call()
+                })
+        )
+
+        return { address: address, requests: requests, requestCount: requestCount };
     }
 
     render() {
@@ -27,3 +40,18 @@ class RequestIndex extends Component {
 }
 
 export default RequestIndex;
+
+/*
+Explanation of Fancy JS
+
+So instead of looping over each index like a novice programmer, we do some fancy JS.
+We get the list of all the requests in one go.
+
+Array(4).fill() would return an empty array with 5 indices from 0 to 4.
+Array(4).fill().map((element, index)) => {index} would return values from 0 upto the
+number mentioned in Array(<number mentioned>) and make a list.
+Therefore the output would be [0, 1, 2, 3, 4]
+
+In our case, we would have the list of requests
+
+*/
