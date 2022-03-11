c53cd9eed1f35d625b5f2b0c0bc6efeea9339c55
Sparsh Dutta <sparsh@Sparshs-MacBook-Pro.local>
2022-03-11 14:54:34 +0530

    Adding Styling to the Rows

--- components/RequestRow.js
+++ components/RequestRow.js
@@ -16,22 +16,23 @@ class RequestRow extends Component {
     };
 
     onFinalize = async () => {
-        const campaign = Campaign(this.props.address)
+        const campaign = Campaign(this.props.address);
         const accounts = await web3.eth.getAccounts();
         await campaign.methods
             .finalizeRequest(this.props.id)
             .send({
                 from: accounts[0]
-            })
+            });
     };
     
     render() {
 
         const { Row, Cell } = Table;
         const { id, request, approversCount } = this.props;
+        const readyToFinalize = request.approvalCount > approversCount / 2;  
 
         return(
-            <Row>
+            <Row disabled={request.complete} positive={readyToFinalize && !request.complete}>
                 <Cell>
                     {id}
                 </Cell>
@@ -48,14 +49,21 @@ class RequestRow extends Component {
                     {request.approvalCount}/{approversCount}
                 </Cell>
                 <Cell>
-                    <Button color='green' basic onClick={this.onApprove}>
-                        Approve
-                    </Button>
+                    {request.complete ? null : (
+                            <Button color='green' basic onClick={this.onApprove}>
+                                Approve
+                            </Button>
+                        )
+                    }
                 </Cell>
                 <Cell>
-                    <Button color='teal' basic onClick={this.onFinalize}>
-                        Finalize
-                    </Button>
+                    {
+                        request.complete ? null : (
+                            <Button color='teal' basic onClick={this.onFinalize}>
+                                Finalize
+                            </Button>
+                        )
+                    }
                 </Cell>
             </Row>
         )
